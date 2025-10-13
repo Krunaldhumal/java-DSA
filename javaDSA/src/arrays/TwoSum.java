ee3cf59b2f283afa32eb386f066049f4924a48fe
krunaldhumal <krunaldhumal7890@gmail.com>
2025-10-13 08:28:43 +0530

    Add Two Sum problem solution

---
+++ javaDSA/src/arrays/TwoSum.java
@@ -0,0 +1,24 @@
+package arrays;
+
+//Problem: Two Sum
+//Link: https://leetcode.com/problems/two-sum/
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		return new int[] {};
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		int[] ans = twoSum(nums, target);
+		System.out.println(ans[0] + ", " + ans[1]);
+	}
+}
